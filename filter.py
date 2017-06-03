a13ed44e70bbed49438fcf9105224e10455dcfab
Jing <liujingyun0209@gmail.com>
2017-06-02 23:29:13 -0400

    multithreading for filtering articles length

--- filter.py
+++ filter.py
@@ -3,11 +3,13 @@ doc_dir = '/home/ml/jliu164/corpus/nyt_corpus/content_annotated/'
 sum_dir = '/home/ml/jliu164/corpus/nyt_corpus/summary_annotated/'
 choice = 'content_annotated'
 
+from multiprocessing.dummy import Pool as ThreadPool
 from corenlpy import AnnotatedText as A
 import numpy as np
 import matplotlib.pyplot as plt
 import re
 import pickle
+import itertools
 
 from tagger_test import group_files
 
@@ -15,61 +17,62 @@ def filter_length(dicts):
 	"""
 	Given a dictionary of (topic : [articles]) pair, show the histogram for topic vs summary vs article length
 	"""
-	dict_filtered = {} #{ topic: (avg length of articles, std length of articles),(avg length of summary,std length of summary ) }
-	doc_mean = []
-	doc_std = []
-	sum_mean = []
-	sum_std = []
-	sum_long = []
-
-	for k,v in dicts.items():
-		print(">> Processing topic: "+k+" with length "+str(len(v)))
-
-		cache_doc = []
-		cache_sum = []
-		long_sum = 0
-
-		for path in v:
-			# check articles
-			doc_path = doc_dir + path+".txt.xml"
-			try:
-				xml_doc = open(doc_path).read()
-			except:
-				print("Cannot Open doc: "+doc_path)
-				pass
-			else:
-				annotated_text = A(xml_doc)
-				cache_doc.append(len(annotated_text.sentences))
-
-			year = re.findall(r'\d+',path)
-			sum_path = sum_dir + year[0]+"summary_annotated/"+year[1]+".txt.xml"
-			try:
-				xml_sum = open(sum_path).read()
-			except:
-				print("Cannot Open summary: "+sum_path)
-				continue
-			else:
-				annotated_sum = A(xml_sum)
-		    	if len(annotated_sum.sentences) > 4:
-		    		long_sum += 1
-		    		cache_sum.append(len(annotated_sum.sentences))
-
-		# get values for topic k
-		cache_doc = np.array(cache_doc)
-		cache_sum = np.array(cache_sum)
-
-		doc_mean.append(np.mean(cache_doc))
-		doc_std.append(np.std(cache_doc))
-		sum_mean.append(np.mean(cache_sum))
-		sum_std.append(np.std(cache_doc))
-		sum_long.append(long_sum)
-
-	print(doc_mean)
-	print(doc_std)
-	print(sum_mean)
-	print(sum_std)
 	
-	return doc_mean, doc_std, sum_mean, sum_std
+	topics = dicts.keys()
+	pool = ThreadPool(4)
+
+	results = pool.map(filter_topic, itertools.izip(dicts.keys(), dicts.values()))
+	# results: [(topic, avg doc length, std, avg summary length, std, #long summary)]
+	return results
+
+
+def filter_topic((k,v)):
+	"""
+	For a (k,v) pair: topic k and a list of files v,
+	return (avg length/std of article/summary, #long summary)
+	"""
+	cache_doc = []
+	cache_sum = []
+	long_sum = 0
+	print("\n>> Processing topic: "+k)
+
+	for path in v:
+		# check articles
+		doc_path = doc_dir + path+".txt.xml"
+		try:
+			xml_doc = open(doc_path).read()
+		except:
+			print("Cannot Open doc: "+doc_path)
+			pass
+		else:
+			annotated_text = A(xml_doc)
+			cache_doc.append(len(annotated_text.sentences))
+		
+		# check summary
+		year = re.findall(r'\d+',path)
+		sum_path = sum_dir + year[0]+"summary_annotated/"+year[1]+".txt.xml"
+		try:
+			xml_sum = open(sum_path).read()
+		except:
+			print("Cannot Open summary: "+sum_path)
+			continue
+		else:
+			annotated_sum = A(xml_sum)
+	    	if len(annotated_sum.sentences) >= 2:
+	    		long_sum += 1
+	    	cache_sum.append(len(annotated_sum.sentences))
+	
+	cache_doc = np.array(cache_doc)
+	cache_sum = np.array(cache_sum)
+
+	if long_sum == 0:
+		print(" No summary has >= 2 sentences! ")
+	
+	return k, np.mean(cache_doc),np.std(cache_doc),np.mean(cache_sum),np.std(cache_sum),long_sum
+
+
+
+
 
 def show_graph(doc_mean, doc_std, sum_mean, sum_std, topics):
 	"""
@@ -101,9 +104,11 @@ def show_graph(doc_mean, doc_std, sum_mean, sum_std, topics):
 	plt.tight_layout()
 	plt.show()
 
+
+
 if __name__ == '__main__':
-	inputs = group_files(2000,2001,400,2000)
+	inputs = group_files(2000,2002,400,3000)
 	print(inputs.keys())
 	
 	result = filter_length(inputs)
-	pickle.dump(result, open("2000-2001result.pkl",'wb'))
+	pickle.dump(result, open("2000-2002result.pkl",'wb'))
